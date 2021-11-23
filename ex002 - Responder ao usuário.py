645cc9fc5f3890e7d01f94d713f00e1fbc947d83
Leonardo Rodrigues <93847413+leorodrigues91@users.noreply.github.com>
2021-11-22 22:19:56 -0300

    Update and rename ex002.py to ex002 - Responder ao usuário.py

---
+++ ex002 - Responder ao usuário.py
@@ -0,0 +1,4 @@
+# Criar um programa que pergunte o nome do usuário e mostre uma mensagem de boas-vindas, após isso.
+
+nome = input('Qual o seu nome? ')
+print(f'Olá, {nome}! É um prazer te conhecer!')
--- ex002.py
+++
@@ -1,2 +0,0 @@
-nome = input('Qual o seu nome? ')
-print(f'Olá, {nome}! É um prazer te conhecer!')
