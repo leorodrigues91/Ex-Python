01b9def65dedfc5044775981a7586f422a69fed0
Leonardo Rodrigues <93847413+leorodrigues91@users.noreply.github.com>
2022-05-26 15:57:22 -0300

    Update ex002 - Responder ao usuário.py

--- ex002 - Responder ao usuário.py
+++ ex002 - Responder ao usuário.py
@@ -1,4 +1,4 @@
-# Criar um programa que pergunte o nome do usuário e mostre uma mensagem de boas-vindas, após isso.
+# Criar um programa que pergunte o nome do usuário e, após isso, mostre uma mensagem de boas-vindas.
 
 nome = input('Qual o seu nome? ')
 print(f'Olá, {nome}! É um prazer te conhecer!')
